df39a02e5ce1aa3818db8db77fbe732ef40ff829
JohnWick1975 <vitalijus.rusakas@gmail.com>
2022-01-30 11:27:51 +0200

    songs_list task

---
+++ 06_songs_list.py
@@ -0,0 +1,48 @@
+# Есть список песен группы Depeche Mode со временем звучания с точносттю до долей минут
+
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+
+# распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+#   Три песни звучат ХХХ минут
+# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+
+songs_time = 0
+songs_time += violator_songs_list[3][1]
+songs_time += violator_songs_list[5][1]
+songs_time += violator_songs_list[8][1]
+
+print('total time of three songs = ', round(songs_time, 2), 'minutes')
+
+# Есть словарь песен группы Depeche Mode
+violator_songs_dict = {
+    'World in My Eyes': 4.76,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+
+# распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
+#   А другие три песни звучат ХХХ минут
+
+songs_time = 0
+songs_time += violator_songs_dict['Sweetest Perfection']
+songs_time += violator_songs_dict['Policy of Truth']
+songs_time += violator_songs_dict['Blue Dress']
+
+print('total time of other three songs = ', round(songs_time, 2), 'minutes')
